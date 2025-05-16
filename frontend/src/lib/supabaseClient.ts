e2f1d13724f7da0b8d180e14cbac72d4d1c3ba79
Vander <vandercaneppelle@outlook.com>
2025-05-15 21:58:28 -0300

    added anon var

--- frontend/src/lib/supabaseClient.ts
+++ frontend/src/lib/supabaseClient.ts
@@ -1,5 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://wpyblxwkiiougygcmjlu.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndweWJseHdraWlvdWd5Z2Ntamx1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDczMzQyNTQsImV4cCI6MjA2MjkxMDI1NH0.r7VdugTjubcREUET90YeoV6YAmrM7yLgwAUoyCLFNGg';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+
 export const supabase = createClient(supabaseUrl, supabaseKey);
